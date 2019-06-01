778ca121fec3cf4c5c1422438c199554822ab4f2
Ben Wheatley <BenWheatley@users.noreply.github.com>
2019-06-01 12:37:26 +0200

    Initial Commit

---
+++ SwiftMapper/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SwiftMapper
+//
+//  Created by Ben Wheatley on 2019/06/01.
+//  Copyright © 2019 Ben Wheatley. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ SwiftMapper/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SwiftMapper
+//
+//  Created by Ben Wheatley on 2019/06/01.
+//  Copyright © 2019 Ben Wheatley. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
